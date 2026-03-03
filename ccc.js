21b6e7b8553c86a04c3f1eabeedf829789ae6fb9
DenPar4455 <dennis.parraga.20@gmail.com>
2026-03-03 15:50:36 -0500

    Started html, css, and js files

---
+++ ccc.js
@@ -0,0 +1,5 @@
+document.getElementById("searchBtn").addEventListener("click", () => {
+    const pText = document.getElementById("cityField");
+    pText.innerHTML = "afdsaf"
+    pText.style.backgroundColor = "green";
+});
