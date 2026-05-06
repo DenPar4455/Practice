9c32f624bb470af4c846cfd767a708e62aa58d4b
DenPar4455 <dennis.parraga.20@gmail.com>
2026-05-06 16:55:19 -0400

    adding space

--- ccc.js
+++ ccc.js
@@ -2,4 +2,5 @@ document.getElementById("searchBtn").addEventListener("click", () => {
     const pText = document.getElementById("cityField");
     pText.innerHTML = "afdsaf"
     pText.style.backgroundColor = "green";
+    
 });
